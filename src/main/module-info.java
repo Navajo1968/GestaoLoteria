c489c6d906ca000079207069cf0d4d90b6955fec
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-03 21:47:54 -0300

    Limpar a estrutura

--- src/main/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.loteriascorp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires poi.ooxml;
-
-    opens com.loteriascorp to javafx.fxml;
-    exports com.loteriascorp;
-}
