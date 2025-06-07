b4a52acf611e48bfce0d1e54237f1259adfae18c
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-06 22:10:58 -0300

    TelaPrincipal

--- src/main/java/com/gestaoloteria/loteria/Main.java
+++ src/main/java/com/gestaoloteria/loteria/Main.java
@@ -1,11 +1,22 @@
 package com.gestaoloteria.loteria;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-@SpringBootApplication
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        MainMenu menu = new MainMenu();
+        Scene scene = new Scene(menu.getRoot(), 500, 400);
+
+        primaryStage.setTitle("Gestão de Loterias");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        launch(args);
     }
 }
