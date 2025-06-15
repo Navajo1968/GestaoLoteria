a7a65014693cc012ed6d1c0e53e11ff0daa53fcb
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-15 20:44:05 -0300

    Importação

--- src/main/java/com/gestaoloteria/loteria/model/FaixaPremiacao.java
+++ src/main/java/com/gestaoloteria/loteria/model/FaixaPremiacao.java
@@ -2,7 +2,9 @@ package com.gestaoloteria.loteria.model;
 
 public class FaixaPremiacao {
     private Integer id;
+    private String nome;
     private String descricao;
+    private int ordem;
     private int acertos;
 
     public Integer getId() {
@@ -11,12 +13,28 @@ public class FaixaPremiacao {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescricao() {
         return descricao;
     }
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public int getOrdem() {
+        return ordem;
+    }
+    public void setOrdem(int ordem) {
+        this.ordem = ordem;
+    }
+
     public int getAcertos() {
         return acertos;
     }
