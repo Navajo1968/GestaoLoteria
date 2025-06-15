67713536403a43461f4367b7b06a1c0d4d898a1a
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-15 20:22:35 -0300

    Importação

--- src/main/java/com/gestaoloteria/loteria/dao/ConexaoBanco.java
+++ src/main/java/com/gestaoloteria/loteria/dao/ConexaoBanco.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class ConexaoBanco {
 
-    private static final String URL = "jdbc:sqlite:gestaoloteria.db"; // Ajuste para seu banco
+    private static final String URL = "jdbc:sqlite:gestaoloteria.db"; // Ajuste se necessário
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL);
