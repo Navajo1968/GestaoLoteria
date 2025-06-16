2788acfcaef7be36b72b26e52958463a799f1aee
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-16 18:18:20 -0300

    Update JogoDAO.java

--- src/main/java/com/gestaoloteria/loteria/dao/JogoDAO.java
+++ src/main/java/com/gestaoloteria/loteria/dao/JogoDAO.java
@@ -10,18 +10,36 @@ import java.util.List;
 public class JogoDAO {
 
     public void inserirJogo(Jogo jogo) throws Exception {
-        String sql = "INSERT INTO jogo (loteria_id, concurso_id, numeros) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO jogo (loteria_id, concurso_id, numero_concurso_previsto, numeros, data_hora, acertos, observacao) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)";
         try (Connection conn = ConexaoBanco.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, jogo.getLoteriaId());
-            ps.setInt(2, jogo.getConcursoId());
-            ps.setString(3, jogo.getNumeros());
+            // concurso_id pode ser null
+            if (jogo.getConcursoId() != null) {
+                ps.setInt(2, jogo.getConcursoId());
+            } else {
+                ps.setNull(2, java.sql.Types.INTEGER);
+            }
+            // numero_concurso_previsto
+            if (jogo.getNumeroConcursoPrevisto() != null) {
+                ps.setInt(3, jogo.getNumeroConcursoPrevisto());
+            } else {
+                ps.setNull(3, java.sql.Types.INTEGER);
+            }
+            ps.setString(4, jogo.getNumeros());
+            // acertos pode ser null
+            if (jogo.getAcertos() != null) {
+                ps.setInt(5, jogo.getAcertos());
+            } else {
+                ps.setNull(5, java.sql.Types.INTEGER);
+            }
+            ps.setString(6, jogo.getObservacao());
             ps.executeUpdate();
         }
     }
 
     public List<Jogo> listarJogos() throws Exception {
-        String sql = "SELECT id, loteria_id, concurso_id, numeros FROM jogo";
+        String sql = "SELECT id, loteria_id, concurso_id, numero_concurso_previsto, numeros, data_hora, acertos, observacao FROM jogo";
         List<Jogo> jogos = new ArrayList<>();
         try (Connection conn = ConexaoBanco.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -30,8 +48,15 @@ public class JogoDAO {
                 Jogo jogo = new Jogo();
                 jogo.setId(rs.getInt("id"));
                 jogo.setLoteriaId(rs.getInt("loteria_id"));
-                jogo.setConcursoId(rs.getInt("concurso_id"));
+                int concursoId = rs.getInt("concurso_id");
+                if (!rs.wasNull()) jogo.setConcursoId(concursoId);
+                int numeroConcursoPrevisto = rs.getInt("numero_concurso_previsto");
+                if (!rs.wasNull()) jogo.setNumeroConcursoPrevisto(numeroConcursoPrevisto);
                 jogo.setNumeros(rs.getString("numeros"));
+                jogo.setDataHora(rs.getTimestamp("data_hora") != null ? rs.getTimestamp("data_hora").toLocalDateTime() : null);
+                Integer acertos = rs.getInt("acertos");
+                if (!rs.wasNull()) jogo.setAcertos(acertos);
+                jogo.setObservacao(rs.getString("observacao"));
                 jogos.add(jogo);
             }
         }
@@ -39,13 +64,28 @@ public class JogoDAO {
     }
 
     public void salvarJogos(List<Jogo> jogos) throws Exception {
-        String sql = "INSERT INTO jogo (loteria_id, concurso_id, numeros) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO jogo (loteria_id, concurso_id, numero_concurso_previsto, numeros, data_hora, acertos, observacao) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)";
         try (Connection conn = ConexaoBanco.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             for (Jogo jogo : jogos) {
                 ps.setInt(1, jogo.getLoteriaId());
-                ps.setInt(2, jogo.getConcursoId());
-                ps.setString(3, jogo.getNumeros());
+                if (jogo.getConcursoId() != null) {
+                    ps.setInt(2, jogo.getConcursoId());
+                } else {
+                    ps.setNull(2, java.sql.Types.INTEGER);
+                }
+                if (jogo.getNumeroConcursoPrevisto() != null) {
+                    ps.setInt(3, jogo.getNumeroConcursoPrevisto());
+                } else {
+                    ps.setNull(3, java.sql.Types.INTEGER);
+                }
+                ps.setString(4, jogo.getNumeros());
+                if (jogo.getAcertos() != null) {
+                    ps.setInt(5, jogo.getAcertos());
+                } else {
+                    ps.setNull(5, java.sql.Types.INTEGER);
+                }
+                ps.setString(6, jogo.getObservacao());
                 ps.addBatch();
             }
             ps.executeBatch();
