56be082b65000405f062302a87ccdc27cec748d4
navajo1968 <rodrigor_araujo@hotmail.com>
2025-06-15 21:30:31 -0300

    Importação

--- src/main/java/com/gestaoloteria/loteria/dao/ConexaoBanco.java
+++ src/main/java/com/gestaoloteria/loteria/dao/ConexaoBanco.java
@@ -1,13 +1,19 @@
-package com.gestaoloteria.loteria.dao;
+package com.seuprojeto.loteria;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.InputStream;
 
 public class ConexaoBanco {
-    private static final String URL = "jdbc:sqlite:gestaoloteria.db";
-
     public static Connection getConnection() throws Exception {
-        Class.forName("org.sqlite.JDBC"); // força registro do driver!
-        return DriverManager.getConnection(URL);
+        Properties props = new Properties();
+        try (InputStream input = ConexaoBanco.class.getClassLoader().getResourceAsStream("db.properties")) {
+            props.load(input);
+        }
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+        return DriverManager.getConnection(url, user, password);
     }
 }
